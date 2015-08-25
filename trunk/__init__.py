faf1e3d5723254f5bde400f924dfa2911f78c67a
Your Name <you@example.com>
2015-08-25 21:53:31 +0600

    init

---
+++ trunk/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+Author:         Xia Kai <xiaket@gmail.com>
+Filename:       __init__.py
+Type:           Meta informatin holder
+Last modified:  2010-03-31 08:39
+
+Description:
+
+"""
+__version__ = "0.0.1"
